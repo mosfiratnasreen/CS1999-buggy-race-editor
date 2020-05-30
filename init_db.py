b66b72f66f39cd6b9569e4d9ec30196062e05e87
mosfiratnasreen <63060265+mosfiratnasreen@users.noreply.github.com>
2020-05-30 21:05:33 +0100

    adding flag_color to all files

--- init_db.py
+++ init_db.py
@@ -27,9 +27,10 @@ con.execute("""
   CREATE TABLE IF NOT EXISTS buggies (
     id                    INTEGER PRIMARY KEY,
     qty_wheels            INTEGER DEFAULT 4,
-    flag_color            VARCHAR(20),
+    flag_color            VARCHAR(20) DEFAULT "white",
     flag_color_secondary  VARCHAR(20),
-    flag_pattern          VARCHAR(20)
+    flag_pattern          VARCHAR(20),
+    power_type            VARCHAR(20)
   )
 
 """)
